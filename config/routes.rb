6f2f20133bbd76a64d7afd40b81c22104ef2abe5
Colton Childers <coltonchilders@gmail.com>
2017-10-02 16:04:13 -0500

    fixed divergent paths issue

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     
     get '/about' => 'messages#about'
     get '/messages/:id/:token' => 'messages#show'
+    get '/home/:id/:token' => 'messages#show'
     get '/messages/:id/:token/auth' => 'messages#auth'
+    get '/home/:id/:token/auth' => 'messages#auth'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
